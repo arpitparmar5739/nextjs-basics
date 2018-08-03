dbf711f4f3d0b579872840c67a43d2cc9eaea622
Arpit <ibox@iBox.local>
2018-08-03 16:10:18 +0530

    Added styling and used replace in Link

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 const Index = () => (
     <div>
-        <Link href="/about">
-            <a>About Page</a>
-        </Link>
+        <div>
+            <Link href="/about">
+                <a style={{ fontSize: 30 }}>About Page</a>
+            </Link>
+        </div>
         <p>Hello Next.js</p>
+        <div>
+            <Link href="/about" replace>
+                <button>About Page Link with replace</button>
+            </Link>
+        </div>
+        <div>
+            <h2>Link Works With Anything</h2>
+            <p>
+                Just like a button, you can place any of your custom React components or even a div within a Link.
+            </p>
+            <p>
+                The only requirement for components placed inside a Link is they should accept an onClick prop.
+            </p>
+        </div>
     </div>
 );
 
