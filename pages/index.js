ae200c69692bdcd800b1826a2605e5401b82bcec
Arpit <ibox@iBox.local>
2018-08-06 11:52:28 +0530

    Styled components using style-jsx and react-markdown

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,40 @@
 import Layout from '../components/MyLayout.js';
-import Link from 'next/link';
-import fetch from 'isomorphic-unfetch';
+import {withRouter} from 'next/router';
+import Markdown from 'react-markdown';
 
-const Index = (props) => (
+export default withRouter((props) => (
   <Layout>
-    <h1>Batman TV Shows</h1>
-    <ul>
-      {props.shows.map(({ show }) => (
-        <li key={show.id}>
-          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-            <a>{show.name}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
+   <h1>{props.router.query.title}</h1>
+   <div className="markdown">
+     <Markdown source={`
+This is our blog post.
+Yes. We can have a [link](/link).
+And we can have a title as well.
+
+### This is a title
+
+And here's the content.
+     `}/>
+   </div>
+   <style jsx global>{`
+     .markdown {
+       font-family: 'Arial';
+     }
+
+     .markdown a {
+       text-decoration: none;
+       color: blue;
+     }
+
+     .markdown a:hover {
+       opacity: 0.6;
+     }
+
+     .markdown h3 {
+       margin: 0;
+       padding: 0;
+       text-transform: uppercase;
+     }
+  `}</style>
   </Layout>
-);
-
-Index.getInitialProps = async function () {
-  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-  const data = await res.json();
-  console.log(`Show data fetched. Count: ${data.length}`);
-
-  return {
-    shows: data
-  };
-};
-
-export default Index;
+));
